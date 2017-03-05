9cab5a7a1bddd13b29795a3eee96dba4fdfd17e6
Nick Clarkson <nick@clarkson.io>
2017-03-05 13:29:24 +0000

    item number

--- assets/js/colourChooser.js
+++ assets/js/colourChooser.js
@@ -2,14 +2,31 @@ function data(id){
   return JSON.parse($('#colour-data').html());
 }
 
+
+function removeBEMClass($el){
+  $el.removeClass(function (index, className) {
+    return (className.match (/(^|\s)shirt__fill__polygon--\S+/g) || []).join(' ');
+  });
+}
+
+// shirt__fill__polygon--
+
 /////////
 
 var colourChooser = (function(){
 
-  var colours = data('#colour-data');
+  var colours = data('#colour-data'),
+      $chooser = $('.js-change-colour'),
+      $fill = $('.js-fill-polygon', $chooser),
+      i = 0;
 
-  console.log(colours);
+  // console.log(colours);
 
-  $('.')
+  $chooser.on('click', function(e){
+    e.preventDefault();
+    removeBEMClass($fill);
+    var colour = colours[i = (++i >= colours.length) ? 0 : i];
+    $fill.addClass('shirt__fill__polygon--'+colour.modifier);
+  });
 
 })();
